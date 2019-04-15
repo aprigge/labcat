dc0c6ed64fe59567c68a1e389f84f57fa943c647
Joshua Gomez <joshuagomez@library.ucla.edu>
2019-04-15 11:38:58 -0700

    Created initial Book model with test Added catalog app to settings Addresses #1

--- labcat/catalog/models.py
+++ labcat/catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+	title = models.CharField(max_length=256)
+	volume = models.CharField(max_length=64, blank=True)
+	callnum = models.CharField(max_length=32, blank=True)
+	barcode = models.CharField(max_length=32, blank=True)
+
+	def __repr__(self):
+		return '<Book {}: {}>'.format(self.id, self.title)
