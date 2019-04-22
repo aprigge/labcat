b764cc069f13bb23392be8dc3b4eb2ad73a68749
Simon Zhou <simon.zhou@ucla.edu>
2019-04-22 09:58:59 -0700

    IS-11: Author model
    Build model for author of publication

--- labcat/catalog/models.py
+++ labcat/catalog/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 
 class Book(models.Model):
-	title = models.CharField(max_length=256)
-	volume = models.CharField(max_length=64, blank=True)
-	callnum = models.CharField(max_length=32, blank=True)
-	barcode = models.CharField(max_length=32, blank=True)
+    title = models.CharField(max_length=256)
+    volume = models.CharField(max_length=64, blank=True)
+    callnum = models.CharField(max_length=32, blank=True)
+    barcode = models.CharField(max_length=32, blank=True)
 
-	def __repr__(self):
-		return '<Book {}: {}>'.format(self.id, self.title)
+    def __repr__(self):
+        return '<Book {}: {}>'.format(self.id, self.title)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=32)
+    middle_name = models.CharField(max_length=32, blank=True)
+    last_name = models.CharField(max_length=32)
+
+    def __repr__(self):
+        return '<Author {}: {} {} {}>'.format(self.id, self.first_name, self.middle_name, self.last_name)
+
+# Create your models here.
--- labcat/catalog/tests.py
+++ labcat/catalog/tests.py
@@ -1,16 +1,26 @@
 from django.test import TestCase
-from .models import Book
+from .models import Book, Author
 
 
 class ModelsTestCase(TestCase):
+    def setUp(self):
+        book = Book.objects.create(
+            title='Test Driven Development: By Example',
+            barcode='12345678910',
+            callnum='TX321.78 J42',
+            volume='')
+        author = Author.objects.create(
+            first_name='Joshua',
+            middle_name='Bruin',
+            last_name='Gomez')
 
-	def setUp(self):
-		book = Book.objects.create(
-			title='Test Driven Development: By Example',
-			barcode='12345678910',
-			callnum='TX321.78 J42',
-			volume='')
+    def test_book_model(self):
+        book = Book.objects.get(barcode='12345678910')
+        self.assertEqual(
+            repr(book), '<Book 1: Test Driven Development: By Example>')
 
-	def test_book_model(self):
-		book = Book.objects.get(barcode='12345678910')
-		self.assertEqual(repr(book), '<Book 1: Test Driven Development: By Example>')
+    def test_author_model(self):
+        author = Author.objects.get(
+            first_name='Joshua', middle_name='Bruin', last_name='Gomez')
+        self.assertEqual(
+            repr(author), '<Author 1: Joshua Bruin Gomez>')
