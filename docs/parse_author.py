c07e94a04eaf77a4d2e2500585343c8cf9f53e2c
Simon Zhou <simon.zhou@ucla.edu>
2019-04-22 18:19:46 -0700

    IS-10: Clean up source data file

---
+++ docs/parse_author.py
@@ -0,0 +1,24 @@
+import os
+import csv
+import sys
+
+res = []
+with open(sys.argv[1], mode='r') as input_file, open("output.csv", mode='w') as output_file:
+    content = csv.reader(input_file, delimiter=',')
+    for row in content:
+        author = None
+        if (len(row) > 8):
+            author = row[8]
+
+        if (author):
+            author = author.replace("&", "|")
+            author = author.replace("and", "|")
+            author = author.replace(";", "|")
+            row[8] = author
+        res.append(row)
+
+    print("Writing to file")
+    output = csv.writer(output_file, delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for rows in res:
+        output.writerow(rows)
