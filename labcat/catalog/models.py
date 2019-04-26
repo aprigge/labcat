b950192c9a0c8260a2eb56e4e0f51058bdcdb772
Joshua Gomez <joshuagomez@library.ucla.edu>
2019-04-26 16:21:30 -0700

    fixed problems with Publisher model and test

--- labcat/catalog/models.py
+++ labcat/catalog/models.py
@@ -2,17 +2,18 @@ from django.db import models
 
 
 class Book(models.Model):
-	title = models.CharField(max_length=256)
-	volume = models.CharField(max_length=64, blank=True)
-	callnum = models.CharField(max_length=32, blank=True)
-	barcode = models.CharField(max_length=32, blank=True)
+    title = models.CharField(max_length=256)
+    volume = models.CharField(max_length=64, blank=True)
+    callnum = models.CharField(max_length=32, blank=True)
+    barcode = models.CharField(max_length=32, blank=True)
 
-	def __repr__(self):
-		return '<Book {}: {}>'.format(self.id, self.title)
+    def __repr__(self):
+        return '<Book {}: {}>'.format(self.id, self.title)
+
 
 class Publisher(models.Model):
-	publisher_name = models.CharField(max_length=256)
-	publisher_city = models.CharField(max_length = 64, blank=True)
+    publisher_name = models.CharField(max_length=256)
+    publisher_city = models.CharField(max_length = 64, blank=True)
 
-	def __repr__(self):
-		return '<{}: {}>'.format(self.publisher_city, self.publisher_name)
+    def __repr__(self):
+        return '<Publisher {}: {}>'.format(self.id, self.publisher_name)
