075c795698960158f28676d4541b399965ce206c
parthdoshi84 <parthdoshi84@gmail.com>
2019-04-28 16:06:04 -0700

    Issue 16: Worked on Author List View

--- labcat/catalog/views.py
+++ labcat/catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
-from .models import Book
+from .models import Book, Author
 from .models import Publisher
 
 
@@ -10,3 +10,8 @@ class BookDetailView(DetailView):
 	
 class PublisherDetailView(DetailView):
 	model = Publisher
+
+class AuthorListView(ListView):
+	model = Author
+	def get_queryset(self):
+        	return self.model.objects.order_by('author')
